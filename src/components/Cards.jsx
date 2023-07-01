f37ff047a1de984fe15674e32bcc468f7888e58f
Joseballer <joseballera@gmail.com>
2023-07-01 14:02:05 -0400

    first commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import Card from "../components/Card";
+const Cards = ({ characters, onClose}) => {
+  return (
+    <div >
+      {characters.map((character) => (
+        <Card
+          id={character.id}
+          name={character.name}
+          status={character.status}
+          species={character.species}
+          gender={character.gender}
+          image={character.image}
+          onClose={onClose}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Cards;
