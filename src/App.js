536f730f1a074bcad8bb59851a402b43c4c15928
Joseballer <joseballera@gmail.com>
2023-07-09 15:40:58 -0400

    react-form

--- src/App.js
+++ src/App.js
@@ -3,20 +3,34 @@ import Nav from "./components/nav/Nav";
 import Cards from "./components/cards/Cards";
 import About from "./components/about/About";
 import Detail from "./components/detail/Detail";
-import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import Form from "./components/form/Form";
+import { useState, useEffect } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+
 function App() {
   const [characters, setCharacters] = useState([]);
+  const navigate = useNavigate();
+  const [access, setAccess] = useState(false);
+  const EMAIL = "ejemplo@gmail.com";
+  const PASSWORD = "1Password";
+
+  useEffect(() => {
+    !access && navigate("/");
+  }, [access]);
+  const login = (userData) => {
+    if (userData.password === PASSWORD && userData.email === EMAIL) {
+      setAccess(true);
+      navigate("/home");
+    }
+  };
 
   const onSearch = (id) => {
-   
     fetch(`https://rickandmortyapi.com/api/character/${id}`)
       .then((res) => res.json())
       .then((data) => {
         if (characters.find((char) => char.id === data.id)) {
           return window.alert("¡Ya has elegido este personaje!");
-        }
-        else if (data.name) {
+        } else if (data.name) {
           setCharacters((oldChars) => [...oldChars, data]);
         } else {
           alert("¡Personaje no encontrado!");
@@ -27,12 +41,14 @@ function App() {
   const onClose = (id) => {
     setCharacters((oldChars) => oldChars.filter((char) => char.id !== id));
   };
+  const { pathname } = useLocation();
   return (
     <div className="App">
-      <Nav onSearch={onSearch} />
+      {pathname !== "/" && <Nav onSearch={onSearch} />}
       <Routes>
+        <Route path="/" element={<Form login={login} />} />
         <Route
-          path="/"
+          path="/home"
           element={<Cards characters={characters} onClose={onClose} />}
         />
         <Route path="/about" element={<About />} />
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = ({ onSearch }) => {
   return (
     <div className={style.container}>
       <SearchBar onSearch={onSearch} />
-      <Link to='/'> Home</Link>
+      <Link to='/home'> Home</Link>
       <Link to='/about'> About</Link>
     </div>
   );
